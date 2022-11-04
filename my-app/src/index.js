4a228701fda5b58c745e86b87eb4a88c981d7c68
hinanaz5 <hina.hrm87@gmail.com>
2022-11-04 19:37:43 +0500

    hu

---
+++ my-app/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<h1>Hello world!</h1>);
+
+
+
+reportWebVitals();
